fabd57f0ec020b05bdb4c9db01d1c9d420ee766a
fintold <98172211+fintold@users.noreply.github.com>
2022-07-17 20:22:22 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+sudo apt-get update 
+
+sudo apt -y upgrade && sudo reboot now
+
+sudo apt update 
+
+sudo apt -y install curl apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update 
+sudo apt -y install git curl kubelet kubeadm kubectl 
+
+sudo apt-mark hold kubelet kubeadm kubectl 
+
+kubectl version 
+kubeadm version 
+
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+sudo swapoff -a 
+
+sudo modprobe overlay 
+sudo modprobe br_netfilter 
+
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1 
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+sudo apt update 
+
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release 
+sudo mkdir -p /etc/apt/keyrings 
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+
+sudo echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update 
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo docker run hello-world
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+sudo tee /etc/docker/deamon.json <<EOF
+{
+	"exec-opts": ["native.cgroupdriver=systemd"],
+	"log-driver": "json-file",
+	"log-opts": {
+		"max-size": "100m"
+	},
+	"storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl deamon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker
+sudo systemctl enable kubelet 
+
+
